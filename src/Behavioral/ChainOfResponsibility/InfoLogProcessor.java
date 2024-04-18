7dd58224188db66872c7618eec1e5e41fb4ae975
Talisha Tanna <tannatalisha@gmail.com>
2024-04-18 23:04:41 +0530

    Behavioral Design Patterns

---
+++ src/Behavioral/ChainOfResponsibility/InfoLogProcessor.java
@@ -0,0 +1,20 @@
+package Behavioral.ChainOfResponsibility;
+
+public class InfoLogProcessor extends LogProcessor {
+
+    InfoLogProcessor(LogProcessor nextlogProcessor)
+    {
+        super(nextlogProcessor);
+    }
+
+    public void log(int logLevel,String message){
+
+        if(logLevel == INFO) {
+            System.out.println("INFO: " + message);
+        } else{
+            super.log(logLevel, message);
+        }
+
+    }
+
+}
